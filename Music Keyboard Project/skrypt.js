6934b37355b30ccab8ba822cced08253fbcff0f3
Oleksandr Melnichuk <80522052+SanyaBeton@users.noreply.github.com>
2021-06-07 23:12:39 +0300

    Add files via upload

---
+++ Music Keyboard Project/skrypt.js
@@ -0,0 +1,35 @@
+var play1 = function () {
+	audio1.play();
+}
+var play2 = function () {
+	audio2.play();
+}
+var play3 = function () {
+	audio3.play();
+}
+var play4 = function () {
+	audio4.play();
+}
+var play5 = function () {
+	audio5.play();
+}
+var play6 = function () {
+	audio6.play();
+}
+var play7 = function () {
+	audio7.play();
+}
+	document.getElementById('button1').addEventListener('click', play1, false);
+	document.getElementById('button2').addEventListener('click', play2, false);
+	document.getElementById('button3').addEventListener('click', play3, false);
+	document.getElementById('button4').addEventListener('click', play4, false);
+	document.getElementById('button5').addEventListener('click', play5, false);
+	document.getElementById('button6').addEventListener('click', play6, false);
+	document.getElementById('button7').addEventListener('click', play7, false);
+var audio1 = document.getElementById('do');
+var audio2 = document.getElementById('re');
+var audio3 = document.getElementById('mi');
+var audio4 = document.getElementById('fa');
+var audio5 = document.getElementById('sol');
+var audio6 = document.getElementById('lja');
+var audio7 = document.getElementById('si');
